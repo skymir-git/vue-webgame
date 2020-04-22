e1b16ef15ecad7ed6c856664e01100324f38f7cc
skymir-git <seratoa@gmail.com>
2020-04-22 13:39:02 +0900

    Initial commit

---
+++ 7.틱택토EventBus/TdComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+    import EventBus from "./EventBus";
+    export default {
+        name: "TdComponent",
+        props: {
+            cellData: String,
+            rowIndex: Number,
+            cellIndex: Number,
+        },
+        methods: {
+            onClickTd() {
+                if (this.cellData) return;
+                // EventBus.$on 호출
+                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 7.틱택토EventBus/TrComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <tr>
+        <td-component v-for="(cellData, index) in rowData" :key="index" :cell-data="cellData" :cell-index="index" :row-index="rowIndex"></td-component>
+    </tr>
+</template>
+
+<script>
+    import TdComponent from './TdComponent'
+
+    export default {
+        name: "TrComponent",
+        props: {
+          rowData: Array,
+          rowIndex: Number,
+        },
+        data() {
+            return {
+                parent: '부모',
+            }
+        },
+        components: {
+            TdComponent,
+        }
+    }
+</script>
+
+<style>
+
+</style>
